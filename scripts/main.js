d2d330014330d2b8b6e71ddf6ebce9d8db8f7c67
Scotty Wyatt <crashy@Scottys-MacBook-Pro.local>
2021-05-07 10:43:16 -0400

    number press works

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
+const $numbers = document.querySelectorAll('.number');
+const $operators = document.querySelectorAll('.operator');
+const $equals = document.querySelector('.equal-sign')
 
+$numbers.forEach(function (pushNumber) {
+  pushNumber.addEventListener('click', function(){
+    alert('')
+  })
+});
+
+
+
+  // .addEventListener('click', function(pushNumber) {
+  //     alert()
+  // })
