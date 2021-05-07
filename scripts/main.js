b8454734e5158e0f1c92f7a801e59332add0836e
Scotty Wyatt <crashy@Scottys-MacBook-Pro.local>
2021-05-07 16:17:20 -0400

    numbers and operators pushed to calculation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,27 @@
 const $numbers = document.querySelectorAll('.number');
-const $operators = document.querySelectorAll('.operator');
+const $operators = document.querySelectorAll('.operator', '.clear');
 const $equals = document.querySelector('.equal-sign')
+var $calculation = []
 
-$numbers.forEach(function (pushNumber) {
+$numbers.forEach(function(pushNumber) {
   pushNumber.addEventListener('click', function(){
     alert(this.innerHTML);
+    $calculation.push(this.value)
+    console.log(calculation)
   })
 });
+
+$operators.forEach(function(pushOperator) {
+  pushOperator.addEventListener('click', function() {
+    alert(this.innerHTML);
+    $calculation.push(this.value)
+    console.log(calculation)
+  })
+});
+
+$equals.addEventListener('click', function(calculate) {
+  alert(this.innerHTML);
+    // for()
+});
+
+// event.target.textContext
