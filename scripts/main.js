83222da2dde97842bd45c29163c6dc01e56b67f6
Scotty Wyatt <crashy@scottys-mbp.attlocal.net>
2021-05-09 21:45:15 -0400

    pushing equal causes an alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,78 @@
 const $numbers = document.querySelectorAll('.number');
 const $operators = document.querySelectorAll('.operator', '.clear');
 const $equals = document.querySelector('.equal-sign')
+const $calculatorScreen = document.querySelector('.calculator-screen')
 var $calculation = []
 
 $numbers.forEach(function(pushNumber) {
   pushNumber.addEventListener('click', function(){
-    alert(this.innerHTML);
+    alert(this.value);
     $calculation.push(this.value)
-    console.log(calculation)
+    console.log($calculation)
   })
 });
 
 $operators.forEach(function(pushOperator) {
   pushOperator.addEventListener('click', function() {
-    alert(this.innerHTML);
+    alert(this.value);
     $calculation.push(this.value)
-    console.log(calculation)
+    console.log($calculation)
   })
 });
 
 $equals.addEventListener('click', function(calculate) {
-  alert(this.innerHTML);
-    // for()
-});
+  alert(this.value);
+  let operator = '0'
 
-// event.target.textContext
+      if(operator == '+') {
+        result = parseFloat(n1) + parseFloat(n2);
+      } else if (operator == '-') {
+        result = parseFloat(n1) - parseFloat(n2);
+      } else if (operator == '*') {
+        result = parseFloat(n1) * parseFloat(n2);
+      } else if (operator == '/') {
+        result = parseFloat(n1) / parseFloat(n2);
+      }
+        alert($calculation)
+})
+
+// for(i = 0; i < $calculation.length, i++)
+//     // let result;
+//     //
+//     // switch($equals) {
+//     //   case '+':
+//     //     result = number1 + number2
+//     //     $calculation.push(this.value)
+//     //     console.log(`${number1} + ${number2} = ${result}`);
+//     //     break;
+//     //   case '-':
+//     //     result = number1 - number2
+//     //     console.log(`number1 + number2 = result`)
+//     //     break;
+//     //   case '*':
+//     //     result = number1 * number2
+//     //     console.log(`number1 * number2 = result`)
+//     //     break;
+//     //   case '/':
+//     //     result = number1 / number2
+//     //     console.log(`number1 / number2 = result`)
+//     //     break;
+//     // }
+//
+//       }
+//     }
+//   });
+//
+// for(i = 0; i < $calculation.length, i++)
+// let operator = '0'
+//
+//     if(operator === '+') {
+//       result = n1 + n2;
+//     } else if (operator === '-') {
+//       result = n1 - n2;
+//     } else if (operator === '*') {
+//       result = n1 * n2;
+//     } else if (operator === '/') {
+//       result = n1 / n2;
+//     }
+//       alert($calculation)
